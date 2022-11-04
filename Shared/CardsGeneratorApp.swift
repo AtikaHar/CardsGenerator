fda347734c049c2402f9f113dee188c50d91f5c5
Atika <haryouli.atika@gmail.com>
2022-11-04 12:50:42 +0100

    Initial Commit

---
+++ Shared/CardsGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsGeneratorApp.swift
+//  Shared
+//
+//  Created by Atika Haryouli on 04/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CardsGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
